730afc6bdae44dc808a4232a7ab8765d0d43f75a
CristiYTRomania <jitescusilviucristian99@gmail.com>
2026-04-07 23:16:51 +0300

    adaugare filtre

---
+++ src/components/HeadphoneJackFilter.jsx
@@ -0,0 +1,16 @@
+function HeadphoneJackFilter(props) {
+  const type = ["Yes", "No", "All"]
+  return (
+    <div style={{ marginBottom: '20px', textAlign: 'center' }}>
+      {
+        type.map((type_)=>(
+            <button key={type_} onClick={() => props.onSelect(type_)}>
+                {type_}
+            </button>
+        ))
+      }
+    </div>
+  );
+}
+
+export default HeadphoneJackFilter;
